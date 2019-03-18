e697e2388af52a5eb3016206271c019485e6f3de
root <root@summit.med.unideb.hu>
2019-03-18 13:51:44 +0100

    Correct pfm.py

--- pfm.py
+++ pfm.py
@@ -3,16 +3,17 @@
 import glob
 import sys
 
-db = glob.glob(sys.argv[1] + "/*.meme")
+db = open(sys.argv[1])
 
-out = open("pfm.tsv", "w")
 for i in db:
-    matrixfile = open(sys.argv[1] + "/" + i)
+    fields = i.rstrip().split()
+    motifid = fields[0]
+    motifname = fields[1]
+    matrixfile = open(sys.argv[2] + "/" + motifname + ".meme")
     counter = 0
     for j in matrixfile:
         mfields = j.rstrip().split()
         if len(mfields) == 4:
             counter = counter + 1
-            out.write("%s\t%s\t%s\t%s\t%s\t%s\n" % (motifid, counter, mfields[0], mfields[1], mfields[2], mfields[3]))
+            print("%s\t%s\t%s\t%s\t%s\t%s" % (motifid, counter, mfields[0], mfields[1], mfields[2], mfields[3]))
     matrixfile.close()
-out.close()
