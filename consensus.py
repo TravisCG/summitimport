e85297cad107faa24c3212ef3d2a9b74b8ea1bb2
root <root@summit.med.unideb.hu>
2019-03-19 15:26:19 +0100

    I have forget to add consensus.py

--- consensus.py
+++ consensus.py
@@ -10,5 +10,5 @@ f.readline()
 count = 1
 for i in f:
 	fields = i.rstrip().split()
-	print(count, fields[0], fields[1], fields[2], fields[3], "/molbio/projects/summitdb/database/motif_info/motif_logos/" + fields[0] + ".pdf", sep = "\t")
+	print(count, fields[0], fields[1], fields[2], fields[3], "/molbio/projects/summitdb/database_2018/motif_info/motif_logos/" + fields[0] + ".pdf", sep = "\t")
 	count += 1
