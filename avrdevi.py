e88dc199cd6793047e1794d7dde80027a8f60cd2
root <root@summit.med.unideb.hu>
2019-03-20 12:52:49 +0100

    New script for average deviation

--- avrdevi.py
+++ avrdevi.py
@@ -20,12 +20,17 @@ for name in sys.argv[3:]:
 	f = open(name)
 	f.readline()
 	for i in f:
-		fields = i.rstrip().split("\t")
-		exp    = fields[0]
+		fields   = i.rstrip().split("\t")
+		exp      = fields[0]
+		parts    = exp.split("_")
+		cellline = parts[2]
+		antibody = parts[4]
+		srx      = parts[5]
+		avgname  = "Motif: %s - Antibody: %s - Cell Type: %s - Experiment: %s" % (motif, antibody, cellline, srx)
 		if exp not in experiment:
 			continue
 		if motif not in consensus:
 			continue
-		print(count, fields[1], fields[2], fields[3], fields[4], experiment[exp], consensus[motif], sep = "\t")
+		print(count, fields[1], fields[2], fields[3], fields[4], experiment[exp], consensus[motif], avgname, sep = "\t")
 		count += 1
 	f.close()
