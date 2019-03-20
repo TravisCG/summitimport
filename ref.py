83edf1effbd57d9db5a188c00ad73a07c189eec2
root <root@summit.med.unideb.hu>
2019-03-20 10:01:05 +0100

    ref.py modified to use the same way as other scripts

--- ref.py
+++ ref.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 import gzip
+import sys
 
 seq = []
-out = open("reference.tsv", "w")
-for i in gzip.open("Homo_sapiens.GRCh37.dna.primary_assembly.fa.gz", "r"):
+for i in gzip.open(sys.argv[1], "r"):
     line = i.decode('utf-8').rstrip()
     if line.startswith(">"):
         if len(seq) > 0:
@@ -13,11 +13,10 @@ for i in gzip.open("Homo_sapiens.GRCh37.dna.primary_assembly.fa.gz", "r"):
                 subseq = seq[j:j+200]
                 if subseq == "N"*200:
                     continue
-                out.write("%s\t%d\t%d\t%s\n" % (header, j+1, j+len(subseq), subseq))
+                print("%s\t%d\t%d\t%s" % (header, j+1, j+len(subseq), subseq))
         seq = []
         header = line.split()[0][1:]
         if header.startswith('K') or header.startswith('G'): # alternative contigs
             break
         continue
     seq.append(line)
-out.close()
