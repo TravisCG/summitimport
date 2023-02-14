44b731adac5c8e6778eecb2f8cb7efa1d86f841e
TravisCG <buldozer88@hotmail.com>
2023-02-14 08:02:40 +0100

    Damn genome versions

--- genome.py
+++ genome.py
@@ -2,5 +2,5 @@
 
 # Right now there is only two genomes
 
-print("1", "hs", "Homo sapiens", "GRCh38", sep = "\t")
+print("1", "hs", "Homo sapiens", "GRCh37", sep = "\t")
 print("2", "mm", "Mus musculus", "GRCm38", sep = "\t")
